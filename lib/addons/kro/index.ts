addeb774506bc82318a2135b822793175a4ee1a9
Zach Jacobson <zjacobso@amazon.com>
2025-09-18 15:36:52 -0700

    kro addon init

---
+++ lib/addons/kro/index.ts
@@ -0,0 +1,44 @@
+import { Construct } from "constructs";
+import { ClusterInfo, Values } from "../../spi";
+import { merge } from "ts-deepmerge";
+import { HelmAddOn, HelmAddOnUserProps } from "../helm-addon"
+
+const KRO_VERSION="0.4.1";
+
+export interface KroAddOnProps extends HelmAddOnUserProps {
+  /**
+   * To Create Namespace using CDK
+   */    
+  createNamespace?: boolean;
+}
+/**
+ * Defaults options for the add-on
+ */
+const defaultProps = {
+  name: "kro",
+  release: "kro",
+  namespace: "kro",
+  chart: "kro",
+  version: KRO_VERSION,
+  repository: "oci://ghcr.io/kro-run/kro/kro",
+  values: {},
+  createNamespace: true
+};
+
+export class KroAddOn extends HelmAddOn {
+
+  readonly options: KroAddOnProps;
+
+  constructor(props?: KroAddOnProps) {
+    super({...defaultProps, ...props});
+    this.options = this.props as KroAddOnProps;
+  }
+  deploy(clusterInfo: ClusterInfo): void | Promise<Construct> {
+    let values: Values = this.options.values ?? {};
+    values = merge(values, this.props.values ?? {});
+    const chart = this.addHelmChart(clusterInfo, values, this.options.createNamespace);
+
+    return Promise.resolve(chart);
+  }
+
+}
