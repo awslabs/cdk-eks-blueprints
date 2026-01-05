927d68bf0353849eab583756bbf6bed1bd28d894
Zachary Jacobson <zjacobso@amazon.com>
2026-01-05 15:12:59 -0800

    fix automode props

--- lib/builders/automode-builder.ts
+++ lib/builders/automode-builder.ts
@@ -5,15 +5,14 @@ import { BlueprintBuilder } from "../stacks";
 import * as addons from '../addons';
 
 const defaultOptions: Partial<AutomodeClusterProviderProps> = {
-  version: eks.KubernetesVersion.V1_31,
   nodePools: ['system', 'general-purpose']
 };
 
 export class AutomodeBuilder extends BlueprintBuilder {
 
-  public static builder(options: Partial<AutomodeClusterProviderProps>): AutomodeBuilder {
+  public static builder(options?: Partial<AutomodeClusterProviderProps>): AutomodeBuilder {
     const builder = new AutomodeBuilder();
-    const mergedOptions = merge(defaultOptions, options);
+    const mergedOptions = {...defaultOptions, ...options}
 
     builder
       .clusterProvider(new AutomodeClusterProvider(mergedOptions))
