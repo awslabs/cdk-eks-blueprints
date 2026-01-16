cd7d05b0d31586c16744cdc4bb7371b3c728a1b7
Zachary Jacobson <zjacobso@amazon.com>
2026-01-15 17:08:40 -0800

    test for secondary subnets

---
+++ examples/secondary-cidr-deps.ts
@@ -0,0 +1,30 @@
+
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import * as blueprints from '../lib';
+import { PrivateSubnet } from 'aws-cdk-lib/aws-ec2';
+
+const app = new cdk.App();
+
+const account = process.env.CDK_DEFAULT_ACCOUNT;
+const region = process.env.CDK_DEFAULT_REGION;
+
+const vpcProvider = new blueprints.VpcProvider(undefined, {
+  primaryCidr: "10.0.0.0/16",
+  secondaryCidr: "100.64.0.0/16",
+  secondarySubnetCidrs: ["100.64.0.0/24", "100.64.1.0/24", "100.64.2.0/24"] // Pod subnets in secondary CIDR
+});
+// Example customer issue reproduction:
+const addOns: Array<blueprints.ClusterAddOn> = [
+  // Add customer's addons here
+];
+
+const stack = blueprints.EksBlueprint.builder()
+  .account(account)
+  .region(region)
+  .resourceProvider(blueprints.GlobalResources.Vpc, vpcProvider)
+  .addOns(...addOns)
+  .version("auto")
+  .build(app, 'debug-stack');
+
+void stack; // Keep for debugging
