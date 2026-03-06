fbc9627a20f7caa473e2938746befc2b3f4b925c
user1 <user1@workshop.local>
2026-03-06 15:58:09 -0500

    fixed unit tests and jest warnings

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,10 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
-  }
+  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/test/test-support.ts'],
+  testTimeout: 300000,
+  maxWorkers: 1
 };
